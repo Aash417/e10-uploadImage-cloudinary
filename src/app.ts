82d78986822c4adf96bfe0d2a4b218445790b143
aashish_k <aashishkathat00@gmail.com>
2024-04-17 17:51:42 +0530

    init

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Express } from 'express';
+import userRouter from './routes/user.routes';
+const app: Express = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: '16kb' }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '16kb',
+  })
+);
+app.use(express.static('public'));
+app.use(cookieParser());
+
+
+// routes declare
+app.use('/', userRouter);
+
+export { app };
+
